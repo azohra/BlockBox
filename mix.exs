5892469d9519fc6bae2e8d4bea883e8a7b72b40c
ssajnani <ssajnani@uwo.ca>
2019-11-18 13:47:49 -0500

    publishing

--- mix.exs
+++ mix.exs
@@ -4,10 +4,22 @@ defmodule Blockbox.MixProject do
   def project do
     [
       app: :blockbox,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.9",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps,
+      package: package()
+    ]
+  end
+
+  defp package do
+    [
+      description: "A tool used to generate slack UI blocks using elixir defined functions.",
+      files: ["lib", "mix.exs", "README.md", "LICENSE*"],
+      maintainers: ["Samar Sajnani"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/azohra/BlockBox"}
     ]
   end
 
@@ -21,6 +33,7 @@ defmodule Blockbox.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
