c0bbc4cc28c85451369961abaabeb40e66a800a4
BORNA HOUMANI-FARAHANI <bhoumani@uwaterloo.ca>
2020-04-25 09:20:30 -0400

    version change

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BlockBox.MixProject do
   def project do
     [
       app: :blockbox,
-      version: "1.1.1",
+      version: "1.1.2",
       elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
